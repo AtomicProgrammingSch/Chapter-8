a0a1d357e5a5ee96ec1dd4ec5e5361821679aeeb
AtomicProgramming <georgetimms2005@outlook.com>
2021-03-21 06:53:17 +0000

    finished task-1a

---
+++ task-1/a.py
@@ -0,0 +1,7 @@
+with open("PrimeMinisters.txt", "w") as f:
+    f.write("Last five UK Prime Minister and term of office\n")
+    f.write("\nBoris Johnson    2019 - incumbent\n")
+    f.write("Theresa May        2016 - 2019\n")
+    f.write("David Cameron      2010 - 2016\n")
+    f.write("Gordon Brown       2007 - 2010\n")
+    f.write("Tony Blair         1997 - 2007\n")
